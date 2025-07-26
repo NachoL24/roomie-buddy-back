e64d3201e4a4c1c00f7a2b23745ff6b2ec44e834
NachoL24 <ignaciolances24@gmail.com>
2025-07-26 15:32:16 -0300

    refactor: update user use case to return RoomieDto and add RoomieDto class

--- src/application/use-cases/user/user.use_case.ts
+++ src/application/use-cases/user/user.use_case.ts
@@ -4,19 +4,20 @@ import { Roomie } from "src/domain/entities";
 import { RoomieRepository } from "src/domain/repositories";
 import { ROOMIE_REPOSITORY } from "src/infrastructure/database/repositories";
 import { Auth0Profile, Auth0UserinfoAdapter } from "src/infrastructure/external-services/auth0/auth0-userInfo.adapter";
+import { RoomieDto } from "src/presentation/dtos/roomie.dto";
 
 @Injectable()
 export class UserUseCase {
 
     constructor(@Inject(ROOMIE_REPOSITORY) private readonly roomieRepository: RoomieRepository, private readonly auth0UserinfoAdapter: Auth0UserinfoAdapter) { }
 
-    async getUserOrCreateUser(auth0Payload: AuthenticatedUserDto): Promise<Roomie> {
+    async getUserOrCreateUser(auth0Payload: AuthenticatedUserDto): Promise<RoomieDto> {
         // 1. Intentar encontrar usuario por Auth0 Sub
         console.log("Fetching user by Auth0 Sub:", auth0Payload.sub);
         let existingUser = await this.roomieRepository.findByAuth0Sub(auth0Payload.sub);
 
         if (existingUser) {
-            return existingUser;
+            return RoomieDto.fromDomain(existingUser);
         }
         console.log("No existing user found by Auth0 Sub, fetching user info...");
         const userInfo = await this.auth0UserinfoAdapter.fetchProfile(auth0Payload.accessToken);
@@ -31,11 +32,17 @@ export class UserUseCase {
         if (existingUser) {
             // Vincular Auth0 Sub al usuario existente
             const updatedUser = this.linkAuth0ToExistingUser(existingUser, auth0Payload.sub);
-            return await this.roomieRepository.save(updatedUser);
+            const savedUser = await this.roomieRepository.save(updatedUser);
+            console.log("Updated existing user with Auth0 Sub:", savedUser);
+            return RoomieDto.fromDomain(savedUser);
         }
 
         // 3. Crear nuevo usuario
-        return await this.createUserFromAuth0(userInfo);
+        const newUser = await this.createUserFromAuth0(userInfo);
+
+        const roomieDto = RoomieDto.fromDomain(newUser);
+        console.log("Created Roomie DTO:", roomieDto);
+        return roomieDto;
     }
 
     async getUserByAuth0Sub(auth0Sub: string): Promise<Roomie | null> {
---
+++ src/presentation/dtos/roomie.dto.ts
@@ -0,0 +1,83 @@
+import { Roomie } from "src/domain/entities/roomie.entity";
+
+export class RoomieDto {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    auth0Sub?: string;
+    profileCompleted: boolean;
+    document?: string;
+    picture?: string;
+
+    constructor(
+        id: number,
+        name: string,
+        surname: string,
+        email: string,
+        profileCompleted: boolean,
+        auth0Sub?: string,
+        document?: string,
+        picture?: string
+    ) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.auth0Sub = auth0Sub;
+        this.profileCompleted = profileCompleted;
+        this.document = document;
+        this.picture = picture;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            surname: this.surname,
+            email: this.email,
+            auth0Sub: this.auth0Sub,
+            profileCompleted: this.profileCompleted,
+            document: this.document,
+            picture: this.picture
+        };
+    }
+
+    static create(
+        name: string,
+        surname: string,
+        email: string,
+        id?: number,
+        auth0Sub?: string,
+        document?: string,
+        picture?: string
+    ): RoomieDto {
+        var profileCompleted = false;
+        if (name && surname && email && document && picture) {
+            profileCompleted = true;
+        }
+        return new RoomieDto(
+            id || 0,
+            name,
+            surname,
+            email,
+            profileCompleted,
+            auth0Sub,
+            document,
+            picture
+        );
+    }
+
+    static fromDomain(roomie: Roomie): RoomieDto {
+        return new RoomieDto(
+            roomie.id,
+            roomie.name,
+            roomie.surname,
+            roomie.email,
+            !!(roomie.name && roomie.surname && roomie.email && roomie.document && roomie.picture),
+            roomie.auth0Sub,
+            roomie.document,
+            roomie.picture
+        );
+    }
+}
