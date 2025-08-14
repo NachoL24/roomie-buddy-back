7268773f9333d5685d3f69fc717f4dfc1946dcb1
NachoL24 <ignaciolances24@gmail.com>
2025-08-14 16:31:29 -0300

    feat: set global API prefix in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors(
     {
       origin: process.env.CORS_ORIGIN || '*',
